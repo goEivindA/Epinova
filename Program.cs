935b0e6cc0b53ac0ea8d9a9eeba7d90efaaf99b7
eivind <98eivindaskeland@gmail.com>
2022-01-31 11:00:58 +0100

    lever del 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EpinovaPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fizzbuzz fizzbuzz = new Fizzbuzz();
+            fizzbuzz.PrintFizzBuzz();
+        }
+    }
+}
